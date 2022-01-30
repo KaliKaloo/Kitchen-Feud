63dc585b104ff162818819ecb62a8fca6ebf6c3d
Jackson Lawrence <fi19710@bristol.ac.uk>
2022-01-30 21:15:23 +0000

    add photon engine

---
+++ kitchen_feud/Assets/menuController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour
+{ 
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+}
+   
+
