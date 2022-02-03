623fd6b1b1eb1fd20e604a92bffe972d33de7a7b
Pragya.Kali <pragyagurung001@gmail.com>
2022-02-03 02:28:45 +0000

    pickup and drop work with prefab now

--- kitchen_feud/Assets/Scripts/PickUpController.cs
+++ kitchen_feud/Assets/Scripts/PickUpController.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PickUpController : MonoBehaviour
 {
     public Rigidbody rb;
     public BoxCollider coll;
-    public Transform player, slot, fpsCam;
-
+    public Transform player;
     public float pickUpRange;
     
     private bool equipped;
     private bool slotFull;
+
     private Vector3 half = new Vector3(0.5f, 0.5f, 0.5f);
+   
+    
     // Update is called once per frame
     void Update()
     {
-        Vector3 distanceToPlayer = player.position - transform.position;
-        if(!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.X) && !slotFull) {
-            PickUp();
-        }
-        else if(equipped && Input.GetKeyDown(KeyCode.X)) {
-            Drop();
-        }
+            if(!equipped && Input.GetKeyDown(KeyCode.X) && !slotFull) {
+                if(player != null){
+                    GameObject p = GameObject.FindWithTag("Player");
+                    player = p.GetComponent<Transform>();
+                 }
+                
+                Vector3 distanceToPlayer = player.position - transform.position;
+                if(distanceToPlayer.magnitude <= pickUpRange){
+                    PickUp();
+                }
+                Debug.Log(player.position);
+            }
+            else if(equipped && Input.GetKeyDown(KeyCode.X)) {
+                Drop();
+            }
     }
     private void PickUp() {
 
         equipped = true;
         slotFull = true;
-        transform.SetParent(slot);
+        transform.parent = GameObject.Find("slot").transform;
         //transform.position = slot.position;
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.Euler(Vector3.zero);
