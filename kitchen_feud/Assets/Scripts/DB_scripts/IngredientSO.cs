73cdf4f83c5fedce7d78b0bba2e71440faf39f4c
Pragya.Kali <pragyagurung001@gmail.com>
2022-02-08 21:22:25 +0000

    added ingredient locaton

--- kitchen_feud/Assets/Scripts/DB_scripts/IngredientSO.cs
+++ kitchen_feud/Assets/Scripts/DB_scripts/IngredientSO.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 
+public enum ItemType{
+    Freezer,
+    Fridge,
+
+    Pantry
+}
+
 [CreateAssetMenu(fileName = "New Ingredient", menuName = "Assets/Ingredient")]
 public class IngredientSO : ScriptableObject
 {
+    
     public string ingredientID;
     public string ingredientName;
+    public ItemType type;
     public bool canPickUp = false;
     public bool canFry = false;
     public bool canCut = false;
