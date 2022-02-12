0bb4db074e52c687e77f6f3670f88dcf6565e9b2
Pragya.Kali <pragyagurung001@gmail.com>
2022-02-12 21:30:52 +0000

    correct dish found. Also cleaned up some code in playerController

--- kitchen_feud/Assets/Scripts/Items/Stove.cs
+++ kitchen_feud/Assets/Scripts/Items/Stove.cs
@@ -3,62 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun; 
 
-//or Interactable
 public class Stove : Interactable
 {
-    public Stove stove;
     GameObject clickedObj;
-    //PlayerHolding playerHold;
-    //Item item;
-     public GameObject inputObj;
-    public List<pickableItem> itemsOnTheStove = new List<pickableItem>();
-
-//questionable
-    void Start()
-	{
-        if(PhotonNetwork.IsConnected) {
-            isStove = true;
-			// cam = Camera.main;
-			//playerHold = GetComponent<PlayerHolding>();
-            //stove = GameObject.Find("Stove 1").GetComponent<Stove>();
-		}
-	} 
-
-    public void Cook() {
-        //popup of the 2d game (new scene?)
-        //get the ingredients: items on the stove list updated in the overloaded cooking(arg)
-        //get the order
-        //propagate that to the scene
+    public GameObject inputObj;
+    public List<IngredientSO> itemsOnTheStove = new List<IngredientSO>();
+    bool foundMatchingDish = false;
+    DishSO foundDish;
+    public override void Interact(){
+ 	    PlayerHolding playerHold = player.GetComponent<PlayerHolding>();
+        if(playerHold.items.Count!=0){
+            addItem(playerHold.heldObj, playerHold);
+        }else{
+            cookDish();
+        }
+    }
+    public void cookDish(){
+        //popup of the 2d game or popup UI
         //lock the stove! One player interacting with it at a time
-        //player has to have a function for interacting with a stove in which it gives it all the stove needs
-        //+possibly also on player side switching to the scene
-        //cooking in the game, return a new object + score (delete the objects put in)
-        Debug.Log("Cooking!");
-        EnterScene("stoveMinigame");
+        //lock player movement when in minigame
+        //after finishing game, instantiate new gameobject dish
+
+        if(foundMatchingDish){
+            // display the type of dish found
+            // display a "cook <dish name>" ui button 
+            Debug.Log("Recipe found: "+foundDish.name + " - "+ foundDish.dishID);
+            EnterScene("stoveMinigame");
+        }
+        else{
+            Debug.Log("Ingredients given do not make a dish");
+        }
 	}
 
-    public void Cook(GameObject heldObjArg, PlayerHolding playerHold) {
-        //null reference
-        Debug.Log(heldObjArg + "is on the stove"); 
-        pickableItem heldObjArgItem = heldObjArg.GetComponent<pickableItem>();
-        itemsOnTheStove.Add(heldObjArgItem);
-        Debug.Log(itemsOnTheStove);
-        playerHold.dropItem();
+    public void addItem(GameObject heldObjArg, PlayerHolding playerHold) {
+        IngredientItem heldObjArgItem = heldObjArg.GetComponent<IngredientItem>();
+        itemsOnTheStove.Add(heldObjArgItem.item);
+		playerHold.dropItem(); 
         Destroy(heldObjArg);
+        checkForDish();
     }
 
-    public bool isStoveFunction(GameObject obj) {
-        Stove stove = obj.GetComponent<Stove>();
-        if (stove == null) {
-            return false;
-        }
-        //null reference
-        if(stove.isStove){
-            clickedObj = obj;
-            return true;
-        } 
-        else{
-            return false;
-        }
+    public void checkForDish(){
+        foundDish = Database.GetDishFromIngredients(itemsOnTheStove);
+        if(foundDish != null)
+            foundMatchingDish = true;
     }
 }
--- kitchen_feud/Assets/Scripts/Items/pickableItem.cs
+++ kitchen_feud/Assets/Scripts/Items/pickableItem.cs
@@ -6,14 +6,17 @@ public class pickableItem : Interactable
     //public Item item;
     public bool canPickUp = true;
     public BaseFood item;
-    // public IngredientSO ingredient;
-    // public DishSO dish;
-
+    PlayerHolding playerHold;
     public override void Interact()
 	{
-        base.Interact();
-       
-	}
+        playerHold = player.GetComponent<PlayerHolding>();
 
-   
+        if(playerHold.items.Count==0){
+            playerHold.pickUpItem(gameObject, item);
+        }
+        else{
+            playerHold.dropItem();
+            playerHold.pickUpItem(gameObject, item);
+        }
+	}
 }
