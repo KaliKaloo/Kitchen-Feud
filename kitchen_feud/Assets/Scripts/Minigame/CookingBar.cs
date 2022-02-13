42a06810744022e9faba5b9f350d89bd39527fe3
kkadzielna <k.kadzielawa00@gmail.com>
2022-02-13 11:55:24 +0000

    basic key controlled slider

--- kitchen_feud/Assets/Scripts/Minigame/CookingBar.cs
+++ kitchen_feud/Assets/Scripts/Minigame/CookingBar.cs
@@ -6,7 +6,16 @@ using UnityEngine.UI;
 public class CookingBar : MonoBehaviour
 {
     public Slider slider;
-    // Start is called before the first frame update
+    public float step;
+
+    void Start () {
+        slider.value = 0;
+    }
+    void Update() {
+        if (Input.GetKey(KeyCode.X)) {
+            slider.value = slider.value + step;
+        }
+    }
     public void SetCookedLevel(int value)
     {
         slider.value = value;
