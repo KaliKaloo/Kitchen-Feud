34c9465beb926be337430853aa492ffbdf600245
hamza772000 <34235048+hamza772000@users.noreply.github.com>
2022-02-15 11:23:19 +0000

    fixing merging multiplayer with interactable stuff

--- kitchen_feud/Assets/Scripts/Items/pickableItem.cs
+++ kitchen_feud/Assets/Scripts/Items/pickableItem.cs
@@ -1,41 +1,23 @@
-
 using UnityEngine;
-using Photon.Pun;
 
 public class pickableItem : Interactable
 {
+    //public Item item;
+    public bool canPickUp = true;
     public BaseFood item;
-	public PhotonPlayer p;
-    // public IngredientSO ingredient;
-    // public DishSO dish;
-
+    PlayerHolding playerHold;
     public override void Interact()
-	{
-		base.Interact();
-       
-	}
+    {
+        playerHold = player.GetComponent<PlayerHolding>();
 
-	[PunRPC]
-	private void SetPlatformAsParent()
-	{
-		
-		{
-			/*
-			
-			this.transform.parent = p.myAvatar.transform;
-			this.transform.localPosition = Vector3.zero;
-			this.transform.localRotation = Quaternion.Euler(Vector3.zero);
-			this.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX |
-				RigidbodyConstraints.FreezePositionZ;
-			
-			Debug.Log(p.name);
-			*/
-		}
-		
-	}
-	[PunRPC]
-	private void SetNullAsParent()
-	{
-		this.transform.parent = null;
-	}
+        if (playerHold.items.Count == 0)
+        {
+            playerHold.pickUpItem(gameObject, item);
+        }
+        else
+        {
+            playerHold.dropItem();
+            playerHold.pickUpItem(gameObject, item);
+        }
+    }
 }
