43729408cd14137838a95b473c7a40764fde1cbb
Pragya.Kali <pragyagurung001@gmail.com>
2022-02-18 17:00:50 +0000

    changed timer back to 100

--- kitchen_feud/Assets/Scripts/scoreController.cs
+++ kitchen_feud/Assets/Scripts/scoreController.cs
@@ -9,7 +9,7 @@ public class GlobalTimer
 {
 
     // SET TIMER HERE !!!!!!
-    private static readonly int time = 10;
+    private static readonly int time = 100;
 
 
     private static int timer = time;
