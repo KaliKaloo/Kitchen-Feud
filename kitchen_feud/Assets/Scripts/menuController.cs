62d2e1bd37886895eb0c538e19c9bdd071c5f94d
Jackson Lawrence <fi19710@bristol.ac.uk>
2022-02-23 19:13:36 +0000

    logic for team select done
    not attached to photon yet

--- kitchen_feud/Assets/Scripts/menuController.cs
+++ kitchen_feud/Assets/Scripts/menuController.cs
@@ -14,7 +14,6 @@ public class menuController : MonoBehaviourPunCallbacks
 
     [SerializeField] private GameObject startLobbyButton;
 
-
     [SerializeField] private InputField usernameInput;
     [SerializeField] private InputField createGameInput;
     [SerializeField] private InputField joinGameInput;
@@ -27,6 +26,10 @@ public class menuController : MonoBehaviourPunCallbacks
     [SerializeField] private Text playerList2;
 
     [SerializeField] private Text lobbyError;
+
+    private int team1 = 2;
+    private int team2 = 1;
+    private int currentTeam = 1;
 
     private void Start()
     {
@@ -149,16 +152,73 @@ public class menuController : MonoBehaviourPunCallbacks
     {
         InitializeLobby(PhotonNetwork.CurrentRoom.ToString());
     }
+
+    public void SwitchToTeam1() {
+        // CHANGE USER TO TEAM 1 HERE
+        currentTeam = 1;
+
+        // JUST TEST DELETE AFTER
+        team2 -= 1;
+        team1 += 1;
+        print(team1);
+        print(team2);
+        this.GetComponent<PhotonView>().RPC("UpdateLobby", RpcTarget.All);
+    }
+
+    public void SwitchToTeam2()
+    {
+        // CHANGE USER TO TEAM 2 HERE
+        currentTeam = 2;
+
+        // JUST TEST DELETE AFTER
+        team1 -= 1;
+        team2 += 1;
+        print(team1);
+        print(team2);
+        this.GetComponent<PhotonView>().RPC("UpdateLobby", RpcTarget.All);
+    }
+
+
+
+
+    // 0 means both balanced, 1 means team 1 can be joined only, 2 means team 2 can be joined only
+    private int CheckTeamBalance()
+    {
+        int team1Added = team1 + 2;
+        int team2Added = team2 + 2;
+        if (((team1Added % team2Added) >= 1) && (team1Added > team2Added))
+            return 2;
+        else if (((team2Added % team1Added) >= 1) && (team2Added > team1Added))
+            return 1;
+        else
+            return 0;
+    }
         
     // Update is called once per frame
     void Update()
     {
         if (lobbyMenu.activeSelf) {
-            if (PhotonNetwork.IsMasterClient) {
+            int balance = CheckTeamBalance();
+            if (balance == 2)
+            {
+                changeTeam1.SetActive(false);
+                changeTeam2.SetActive(true);
+            } else if (balance == 1)
+            {
+                changeTeam1.SetActive(true);
+                changeTeam2.SetActive(false);
+            } else
+            {
                 changeTeam1.SetActive(false);
                 changeTeam2.SetActive(false);
             }
         }
+    }
+
+    [PunRPC]
+    void UpdateLobby()
+    {
+        InitializeLobby(PhotonNetwork.CurrentRoom.ToString());
     }
 }
 
