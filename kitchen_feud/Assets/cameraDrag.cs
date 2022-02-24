961f8006d74a9d6aac0aed28f1b758b5857c14a2
lokhei <lokhei01@gmail.com>
2022-02-24 04:43:24 +0000

    fix rotation on right drag

--- kitchen_feud/Assets/cameraDrag.cs
+++ kitchen_feud/Assets/cameraDrag.cs
@@ -5,33 +5,20 @@ using UnityEngine;
 public class cameraDrag : MonoBehaviour
 {
     float speed = 5.0f, yaw = 0.0f, pitch = 0.0f;
+    private Quaternion currentRotation;
 
-    Quaternion currentRotation;
-    Vector3 currentEulerAngles;
-
-    void Start()
-    {
-        ResetCamera = Camera.main.transform.eulerAngles;
-        Debug.Log(ResetCamera);
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
          if (Input.GetMouseButton(1)) //start dragging
         {
             yaw += speed * Input.GetAxis("Mouse X");
             pitch -= speed * Input.GetAxis("Mouse Y");
-            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
-            // transform.Rotate(0, -rotatespeed * Time.deltaTime, 0);
-            // currentRotation.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-            // transform.rotation = currentRotation;
+            currentRotation.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            transform.localRotation = currentRotation;
         }
 
         if (Input.GetMouseButton(2)){
             transform.localRotation = Quaternion.identity;
-
         }
     }
 }
