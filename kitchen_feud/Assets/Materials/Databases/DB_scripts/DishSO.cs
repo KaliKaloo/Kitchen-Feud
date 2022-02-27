708bce56be71616692ae1be622d677fa82d1f8d3
Linda <fv19959@bristol.ac.uk>
2022-02-27 14:15:29 +0000

    design done, fixed minigame ui

---
+++ kitchen_feud/Assets/Materials/Databases/DB_scripts/DishSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dish", menuName = "Assets/Dish")]
+public class DishSO : BaseFood
+{
+    //dishID is made by combining the indgredientIDs of ingredients take make the dish
+    public string dishID;
+    public string toCook;
+    public int cookingTime;
+
+    // public int maxScore;
+    // public int finalScore;
+    public List<IngredientSO> recipe = new List<IngredientSO>();
+
+    public List<IngredientSO> GetIngredients()
+    {
+        return recipe;
+    }
+
+    // public GameObject ticket;
+
+}
+
