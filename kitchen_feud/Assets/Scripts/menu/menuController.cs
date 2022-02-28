ae845d9dbb7275cc8ad8edd359e09f6749ae1667
hamza772000 <34235048+hamza772000@users.noreply.github.com>
2022-02-28 18:43:11 +0000

    update

--- kitchen_feud/Assets/Scripts/menu/menuController.cs
+++ kitchen_feud/Assets/Scripts/menu/menuController.cs
@@ -266,7 +266,7 @@ public class menuController : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.CurrentRoom.PlayerCount <= 4)
             //LoadScene(1);
-            //this.GetComponent<PhotonView>().RPC("loadS", RpcTarget.All, 1);
+            //this.GetComponent<PhotonView>().RPC("loadS", RpcTarget.Others, 1);
             LoadScene(1);
         //else
             // change this to load larger kitchen if > 4 players!!!!!
