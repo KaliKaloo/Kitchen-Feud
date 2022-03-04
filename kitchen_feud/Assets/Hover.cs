8783eded7c1797b7dc9d37d201bada4a25149477
hamza772000 <34235048+hamza772000@users.noreply.github.com>
2022-03-04 14:42:14 +0000

    tooltip showing for specific dish on order
    Added dictionary so dish sprite will be automatically detected

--- kitchen_feud/Assets/Hover.cs
+++ kitchen_feud/Assets/Hover.cs
@@ -9,30 +9,35 @@ public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 //public class Hover : MonoBehaviour
 {
     public GameObject tooltip;
-    //public TextMesh text;
     public TextMeshProUGUI text;
     private SpriteRenderer sprite;
-    
-    //public RectTransform background;
     private bool mouse_over = false;
+    public TextMeshProUGUI dish;
+    IDictionary<string, int> dishes = new Dictionary<string, int>();
 
-   /* private void OnMouseOver()
-    {
-        text.text = gameObject.GetComponent<DisplayTicket>().orderMainText.text;
-        tooltip.SetActive(true);
-        Debug.LogError("HELLO");
+    void Start() {
+        dishes.Add("Burger", 1);
+        dishes.Add("Chips", 2);
+        dishes.Add("Egg-fried Rice", 3);
+        dishes.Add("Eggy Bread", 4);
+        dishes.Add("Mushroom Soup", 5);
+        dishes.Add("Omelette", 6);
+        dishes.Add("Salad", 7);
+        dishes.Add("Sandwich", 8);
+        dishes.Add("Tomato Soup", 9);
+
+
     }
-    private void OnMouseExit() 
-    {
-        tooltip.SetActive(false);
-    }*/
+
+
     void Update()
     {
         if (mouse_over)
         {
-            Debug.LogError("Mouse Over");
+            Debug.LogError(dishes[dish.text]);
 
-            text.text = "<sprite=" +0+ ">" + " + " + "<sprite=" + 1 + ">"+ " = " + "<sprite=" + 2 + ">";
+            //text.text = "<sprite=" +0+ ">" + " + " + "<sprite=" + 1 + ">"+ " = " + "<sprite=" + 2 + ">";
+            text.text = "<sprite=" + dishes[dish.text] + ">";
             //text.text = "How does this work? Can you pleasee explain this to me?!?!?";
             //float textPadding = 4f;
             //Vector2 backgroundSize = new Vector2(text.preferredWidth + textPadding*2f,text.preferredHeight + textPadding * 2f);
