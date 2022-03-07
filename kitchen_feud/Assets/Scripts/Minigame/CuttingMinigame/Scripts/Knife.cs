678236c7b045b540303cc4b22906d4b64bd282ec
Linda <fv19959@bristol.ac.uk>
2022-03-07 15:21:55 +0000

    cut appears

--- kitchen_feud/Assets/Scripts/Minigame/CuttingMinigame/Scripts/Knife.cs
+++ kitchen_feud/Assets/Scripts/Minigame/CuttingMinigame/Scripts/Knife.cs
@@ -15,32 +15,37 @@ public class Knife : MonoBehaviour
     void Update()
     {
         //input.GetMouseButtonDown returns true the momement mouse pressed down
-        if (Input.GetMouseButtonDown (0)){
+        if (Input.GetMouseButtonDown(0))
+        {
             this.dragging = true;
-            //to get the unity coordinates we need to access the camera
-            swipeStart = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-        } else if (Input.GetMouseButtonUp (0) && this.dragging) {
+            //to get the unity coordinates we need to access the camera\
+            swipeStart = Input.mousePosition;
+            //swipeStart = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        }
+        else if (Input.GetMouseButtonUp(0) && this.dragging)
+        {
             this.dragging = false;
             this.SpawnCut();
         }
     }
 
-    private void SpawnCut(){
-        Vector2 swipeEnd = Camera.main.ScreenToWorldPoint (Input.mousePosition); 
-        
+    private void SpawnCut()
+    {
+        //Vector2 swipeEnd = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        Vector2 swipeEnd = Input.mousePosition;
         //instantiate and set position of line renderer
         GameObject cutInstance = Instantiate(this.cutPrefab, this.swipeStart, Quaternion.identity);
-        cutInstance.GetComponent<LineRenderer> ().SetPosition(0, this.swipeStart);    
-        cutInstance.GetComponent<LineRenderer> ().SetPosition(1, swipeEnd);   
+        cutInstance.GetComponent<LineRenderer>().SetPosition(0, this.swipeStart);
+        cutInstance.GetComponent<LineRenderer>().SetPosition(1, swipeEnd);
 
         //change how collider of line works
         //the colliders position system is relative to where we start drawing the cut
-        Vector2[] colliderPoints = new Vector2[2]; 
+        Vector2[] colliderPoints = new Vector2[2];
         //colliderPoints[0] = Vector2.zero;
         colliderPoints[0] = Vector2.zero;
         colliderPoints[1] = swipeEnd - swipeStart;
-        cutInstance.GetComponent<EdgeCollider2D> ().points = colliderPoints;
-        
+        cutInstance.GetComponent<EdgeCollider2D>().points = colliderPoints;
+
         Destroy(cutInstance, this.cutLifetime);
     }
 }
