2551049ddccb17a6a7831c19a94349a1df48eebc
Karolina Kadzielawa <k.kadzielawa00@gmail.com>
2022-03-13 12:25:15 +0100

    worksss

--- kitchen_feud/Assets/Scripts/DoorController.cs
+++ kitchen_feud/Assets/Scripts/DoorController.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public int id;
+    public float pos;
     // Start is called before the first frame update
     void Start()
     {
         GameEvents.current.openDoor += openDoorUp;
         GameEvents.current.closeDoor += openDoorDown;
+        pos = this.transform.localPosition.y;
     }
 
     // Update is called once per frame
     private void openDoorUp(int id) {
         if(id == this.id) {
             Debug.Log("Door up!");
-            LeanTween.moveLocalY(gameObject, this.transform.localPosition.y + 2.28f, 1f).setEaseOutQuad();
+            LeanTween.moveLocalY(gameObject, /*this.transform.localPosition.y +*/pos + 6f, 1f).setEaseOutQuad();
         }
     }
     private void openDoorDown(int id) {
         if(id == this.id) {
             Debug.Log("Door down!");
-            LeanTween.moveLocalY(gameObject, this.transform.localPosition.y - 2.28f, 1f).setEaseOutQuad();
+            LeanTween.moveLocalY(gameObject, pos/*this.transform.localPosition.y - 6f*/, 1f).setEaseOutQuad();
         }
     }
 }
