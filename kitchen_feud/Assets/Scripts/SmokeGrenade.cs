cbe961a1b86c5763f3fd3ef2ae02b8f1afdee289
Jackson Lawrence <fi19710@bristol.ac.uk>
2022-03-16 15:42:56 +0000

    smoke on detonation

---
+++ kitchen_feud/Assets/Scripts/SmokeGrenade.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeGrenade : MonoBehaviour
+{
+
+    public float delay = 3f;
+
+    float countdown;
+
+    public GameObject smoke1;
+
+
+    private ParticleSystem particle1;
+
+    bool hasExploded = false;
+    bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle1 = smoke1.GetComponent<ParticleSystem>();
+
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+
+        if (!particle1.isEmitting && hasExploded && started)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Explode()
+    {
+        particle1.Play();
+        started = true;
+    }
+}
