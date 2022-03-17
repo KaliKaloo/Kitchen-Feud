df50486f27971a3e8a5e3f9a7c521d31104b5ac0
lokhei <lokhei01@gmail.com>
2022-03-17 22:13:13 +0000

    test greeting

--- kitchen_feud/Assets/Tests/PlayModeTests/mainMenu.cs
+++ kitchen_feud/Assets/Tests/PlayModeTests/mainMenu.cs
@@ -15,25 +15,6 @@ public class mainMenu
     {
         SceneManager.LoadScene("mainMenu");
     }
-   
-
-    [UnityTest]
-    public IEnumerator UsernameUI()
-    {
-        GameObject usrCanvas = GameObject.Find("usernameCanvas");
-        GameObject userInput = GameObject.Find("usernameInput");
-        Assert.IsNotNull(userInput, "Missing field");
-        userInput.GetComponent<InputField>().text = "user";
-
-        GameObject playButton = GameObject.Find("startGame");
-        Assert.IsNotNull(playButton, "Missing play Button");
-
-        playButton.GetComponent<Button>().onClick.Invoke();
-        Assert.IsTrue(GameObject.Find("mainMenuCanvas") != null && GameObject.Find("mainMenuCanvas").activeSelf);
-
-        yield return null;
-    }
-
 
     [UnityTest]
     public IEnumerator mainMenuLoad()
@@ -41,6 +22,25 @@ public class mainMenu
         Assert.IsFalse(GameObject.Find("LoadingScreen") != null && GameObject.Find("LoadingScreen").activeSelf);
         Assert.IsTrue(GameObject.Find("usernameCanvas").activeSelf);
         Assert.IsFalse(GameObject.Find("startGame") != null && GameObject.Find("startGame").activeSelf);
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator UsernameUI()
+    {
+        GameObject usrCanvas = GameObject.Find("usernameCanvas");
+        GameObject userInput = GameObject.Find("usernameInput");
+        Assert.IsNotNull(userInput, "Missing field");
+        userInput.GetComponent<InputField>().text = "user";
+        GameObject playButton = GameObject.Find("startGame");
+        Assert.IsNotNull(playButton, "Missing play Button");
+        playButton.GetComponent<Button>().onClick.Invoke();
+        Assert.IsTrue(GameObject.Find("mainMenuCanvas") != null && GameObject.Find("mainMenuCanvas").activeSelf);
+        Assert.IsNull(GameObject.Find("usernameCanvas"));
+        Assert.AreEqual("user", PhotonNetwork.NickName);
+        string greeting = GameObject.Find("greetingMessage").GetComponent<Text>().text;
+        Assert.AreEqual("Welcome user!", greeting);
+
         yield return null;
     }
 }
