156af994af25ab85f60b7f9bc7908caf7fbb302e
Pragya.Kali <pragyagurung001@gmail.com>
2022-03-24 18:56:18 +0000

    fixed stove reset

--- kitchen_feud/Assets/Scripts/Minigame/StoveMinigame/ExitStoveMinigame.cs
+++ kitchen_feud/Assets/Scripts/Minigame/StoveMinigame/ExitStoveMinigame.cs
@@ -33,12 +33,16 @@ public class ExitStoveMinigame : MonoBehaviour
 
 	void TaskOnClick(){
 		GameEvents.current.assignPointsEventFunction();
-		pot.transform.position = new Vector2(Screen.width / 2, Screen.height / 4.3f);
 		score.text = "Score: 0/15";
 		stoveScore.ResetValues();
 		backButton.SetActive(false);
 		startButton.SetActive(true);
 		topBar.SetActive(true);
+		var xBound = Screen.width / 2;
+        var lowerBound = pot.position.x - xBound;
+        var upperBound = pot.position.x + xBound;
+        pot.position = new Vector3(Screen.width / 2, Screen.height / 4.3f,0);
+
 		canvas.gameObject.SetActive(true);
 		minigameCanvas.gameObject.SetActive(false);
 
