1c14ced452339f5349c47ac6daa6f63670663bfb
Linda <fv19959@bristol.ac.uk>
2022-03-28 15:32:02 +0100

    not working

--- kitchen_feud/Assets/Scripts/Canvas/OutlineEffect.cs
+++ kitchen_feud/Assets/Scripts/Canvas/OutlineEffect.cs
@@ -10,24 +10,40 @@ using Photon.Pun;
         [ColorUsage(true, true)]
         public Color colorOutline;
         private Renderer rend;
+       public GameObject outlineObject;
         // Start is called before the first frame update
         void Start()
         {
         
-            GameObject outlineObject = Instantiate(this.gameObject,transform.position,transform.rotation,transform);
-            outlineObject.transform.localScale = new Vector3(1, 1, 1);
-            outlineObject.GetComponent<PhotonView>().enabled = false;
-            //Destroy(outlineObject.GetComponent<PhotonView>());
-            Renderer rend = outlineObject.GetComponent<Renderer>();
+            //GameObject outlineObject = Instantiate(this.gameObject,transform.position,transform.rotation,transform);
+            // GameObject outlineObjectCopy = this.gameObject;
+           
+            // PhotonView pv = outlineObjectCopy.GetComponent<PhotonView>();
+            // pv.enabled = false;
+
+            outlineObject = Instantiate(this.gameObject,transform.position,transform.rotation,transform);
+            //  Debug.Log(outlineObject.name);
+            //  outlineObject.transform.SetParent(gameObject.transform);
+            //  outlineObject.transform.localScale = new Vector3(1, 1, 1);
+            // //Destroy(outlineObject.GetComponent<PhotonView>());
+            // Renderer rend = outlineObject.GetComponent<Renderer>();
             rend.material = mat;
             rend.material.SetFloat("_Thickness", thickness);
             rend.material.SetColor("_OutlineColor", colorOutline);
             rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
             rend.enabled = false;
-            outlineObject.GetComponent<Collider>().enabled = false;
-            outlineObject.GetComponent<OutlineEffect>().enabled = false;
+            // outlineObject.GetComponent<Collider>().enabled = false;
+            // outlineObject.GetComponent<OutlineEffect>().enabled = false;
             this.rend = rend;
             rend.enabled = true;
+            
+        }
+        void Update(){
+            if(!outlineObject){
+            outlineObject = PhotonNetwork.Instantiate("test",new Vector3(0,0,0),Quaternion.identity);
+            Debug.Log(outlineObject.name);
+
+            }
         }
         // private void OnMouseExit()
         // {
@@ -35,12 +51,12 @@ using Photon.Pun;
         // }
         // private void OnMouseEnter()
         // {
-            
+        //    rend.enabled = true;
         // }
         // // Update is called once per frame
         // void Update()
         // {
             
-        //}
+        // }
     }
 // }
