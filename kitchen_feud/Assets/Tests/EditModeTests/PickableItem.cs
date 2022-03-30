8d3f2b95b830c8d679d878a062139525d7ae1909
lokhei <lokhei01@gmail.com>
2022-03-30 02:02:06 +0100

    pickable item test

---
+++ kitchen_feud/Assets/Tests/EditModeTests/PickableItem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PickableItem
+{
+
+    pickableItem item;
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        GameObject obj = new GameObject();
+        item = obj.AddComponent<pickableItem>();
+
+    }
+
+    [Test]
+    public void trayInit()
+    {
+        Assert.AreEqual(false, item.onTray, "on Tray should be initialised to false");
+    }
+
+    
+    [Test]
+    public void applianceInit()
+    {
+        Assert.AreEqual(false, item.onAppliance, "on Appliance not initialised to false");
+    }
+
+
+    [Test]
+    public void removeItemFromTray()
+    {
+        item.item = ScriptableObject.CreateInstance<IngredientSO>();
+        TraySO tray = ScriptableObject.CreateInstance<TraySO>();
+        tray.ServingTray.Add(item.item);
+        Assert.AreEqual(1, tray.ServingTray.Count, "serving tray should have 1 ingredient");
+
+        item.removeFromTray(tray);
+        Assert.AreEqual(false, item.onTray, "on Tray should be set to false");
+        Assert.AreEqual(0, tray.ServingTray.Count, "serving tray should be empty");
+    }
+
+  
+}
