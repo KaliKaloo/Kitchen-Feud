12ab54a4549cd8c2eed5f71d85847f696680e3f1
lokhei <lokhei01@gmail.com>
2022-03-30 03:50:20 +0100

    smoke and pick up tests

---
+++ kitchen_feud/Assets/Tests/EditModeTests/EnableSmokeTest.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EnableSmokeTest
+{
+
+    EnableSmoke enableSmoke = new EnableSmoke();
+    
+    
+    [Test]
+    public void InitSmoke()
+    {   
+        enableSmoke = new EnableSmoke();
+        Assert.IsFalse(EnableSmoke.inEnemyKitchen);
+        EnableSmoke.inEnemyKitchen = true;
+    }
+
+
+    [Test]
+    public void RestartState()
+    {   
+        enableSmoke.RestartState();
+        Assert.IsFalse(EnableSmoke.inEnemyKitchen);
+        Assert.IsFalse(EnableSmoke.usedUp);
+
+    }
+
+    [Test]
+    public void UseSmoke()
+    {   
+        enableSmoke.UseSmoke();
+        Assert.IsTrue(EnableSmoke.usedUp);
+    }
+
+
+    [Test]
+    public void SetUIForSmoke()
+    {   
+        GameObject slot = new GameObject();
+        enableSmoke.SetUIForSmoke(slot);
+        Assert.AreEqual(slot, EnableSmoke.smokeSlot);
+        Assert.IsFalse(EnableSmoke.smokeSlot.activeSelf);
+
+    }
+
+    [Test]
+    public void DisableSmokeSlot()
+    {   
+        GameObject slot = new GameObject();
+        enableSmoke.SetUIForSmoke(slot);
+        EnableSmoke.smokeSlot.SetActive(true);
+        enableSmoke.DisableSmokeSlot();
+        Assert.IsFalse(EnableSmoke.smokeSlot.activeSelf);
+    }
+
+
+    [Test]
+    public void NoChangePlayerState()
+    {   
+        EnableSmoke.usedUp = true;
+        bool prevState = enableSmoke.GetPlayerState();
+        enableSmoke.ChangePlayerState(true);
+        Assert.AreEqual(prevState, enableSmoke.GetPlayerState());
+    }
+
+    [Test]
+    public void ChangePlayerStateTrue()
+    {   
+        GameObject slot = new GameObject();
+        enableSmoke.SetUIForSmoke(slot);
+        EnableSmoke.usedUp = false;
+        enableSmoke.ChangePlayerState(true);
+        Assert.IsTrue(enableSmoke.GetPlayerState());
+        Assert.IsTrue(EnableSmoke.smokeSlot.activeSelf);
+    }
+
+
+    [Test]
+    public void ChangePlayerStateFalse()
+    {   
+        GameObject slot = new GameObject();
+        enableSmoke.SetUIForSmoke(slot);
+        EnableSmoke.usedUp = false;
+        enableSmoke.ChangePlayerState(false);
+        Assert.IsFalse(enableSmoke.GetPlayerState());
+        Assert.IsFalse(EnableSmoke.smokeSlot.activeSelf);
+
+    }
+
+    [TearDown]
+
+    public void reset(){
+        EnableSmoke.inEnemyKitchen = false;
+        EnableSmoke.usedUp = false;
+    }
+
+  
+}
---
+++ kitchen_feud/Assets/Tests/EditModeTests/PickUpLockTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PickUpLockTest
+{
+    PickupLock pickup = new PickupLock();
+
+    [Test]
+    public void InitPickup()
+    {
+        Assert.IsFalse(pickup.GetLock(), "pickupLock should be initialised to false");
+    }
+
+
+    [Test]
+    public void Lock()
+    {
+        pickup.Lock();
+        Assert.IsTrue(pickup.GetLock(), "pickupLock should be updated to true");
+    }
+
+    [Test]
+    public void UnLock()
+    {
+        pickup.Lock();
+        pickup.Unlock();
+        Assert.IsFalse(pickup.GetLock(), "pickupLock should be updated to false");
+    }
+
+   
+}
