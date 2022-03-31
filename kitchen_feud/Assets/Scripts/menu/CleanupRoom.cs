afc2640fb56b2452f320163f1e6c5c7641eb9341
Jackson Lawrence <fi19710@bristol.ac.uk>
2022-03-31 16:44:14 +0100

    ignore mushroom just delete later

--- kitchen_feud/Assets/Scripts/menu/CleanupRoom.cs
+++ kitchen_feud/Assets/Scripts/menu/CleanupRoom.cs
@@ -10,25 +10,17 @@ public class CleanupRoom : MonoBehaviour
     // put all things you need resetting/destroyed here
     public void Clean()
     {
-        // DestroyMushrooms();
         CleanPlayerSlots();
     }
-
-    // private void DestroyMushrooms() {
-    //     Destroy(PhotonView.Find(55).gameObject);
-    //     Destroy(PhotonView.Find(56).gameObject);
-    // }
     
     // Destroy all slots player is holding
     private void CleanPlayerSlots() {
         GameObject localPlayer = GameObject.Find("Local");
         // Destroy all items player is holding when game ends
         Transform slots = localPlayer.transform.Find("slot");
-        foreach (Transform child in slots) {
-            if(child.GetComponent<PhotonView>().ViewID != 55 ||child.GetComponent<PhotonView>().ViewID != 56 ){
+        foreach (Transform child in slots) 
+        {
             Destroy(child.gameObject);
-
-            }
         }
         localPlayer = null;
     }
