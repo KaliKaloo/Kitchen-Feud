5210c77a01fe3bde20b030df7a661a5c7aa76a41
lokhei <lokhei01@gmail.com>
2022-04-01 00:49:52 +0100

    finish timer tests

--- kitchen_feud/Assets/Tests/EditModeTests/General/Timer.cs
+++ kitchen_feud/Assets/Tests/EditModeTests/General/Timer.cs
@@ -14,6 +14,8 @@ public class TimerTests
     {
         int currTime = timer.GetCurrentTime();
         Assert.NotNull(currTime, "time should not be null");
+        Assert.AreEqual(timer.GetTotalTime(), currTime, "time should not be null");
+
     }
 
 
@@ -21,24 +23,26 @@ public class TimerTests
     public void ChangeTimerZero()
     {
         timer.ChangeTimerValue(0);
-        int currTime = timer.GetCurrentTime();
-        Assert.AreEqual(0, currTime, "time should be set to 0");
+        Assert.AreEqual(0, timer.GetCurrentTime(), "time should be set to 0");
+        Assert.AreEqual(0, timer.GetTotalTime(), "time should be set to 0");
+
     }
 
     [Test]
     public void ChangeTimerValue()
     {
         timer.ChangeTimerValue(300);
-        int currTime = timer.GetCurrentTime();
-        Assert.AreEqual(300, currTime, "changing timer values does not update correctly");
+        Assert.AreEqual(300, timer.GetCurrentTime(), "changing timer values does not update correctly");
+        Assert.AreEqual(300, timer.GetTotalTime());
     }
 
     [Test]
     public void ChangeTimerNeg()
     {
         timer.ChangeTimerValue(-5);
-        int currTime = timer.GetCurrentTime();
-        Assert.AreEqual(0, currTime, "time should be 0 if set to negative");
+        Assert.AreEqual(0, timer.GetCurrentTime(), "time should be 0 if set to negative");
+        Assert.AreEqual(0, timer.GetTotalTime());
+
     }
 
     [Test]
@@ -61,6 +65,129 @@ public class TimerTests
     {
         string mins = timer.ConvertSecondToMinutes(330);
         Assert.AreEqual("05:30", mins, "seconds not converted to minutes correctly");
+    }
+
+
+    [Test]
+    public void GetCurrentTimeString()
+    {
+        timer.ChangeTimerValue(300);
+        string timeString = timer.GetCurrentTimeString();
+        Assert.AreEqual("05:00", timeString);
+    }
+
+
+    
+    [Test]
+    public void AddShort()
+    {
+        timer.ChangeTimerValue(20);
+        int val = timer.AddSubtractTimerValue(10);
+        Assert.AreEqual(1, val);
+        Assert.AreEqual(20, timer.GetTime());
+        Assert.AreEqual(20, timer.GetTotalTime());
+    }
+
+
+    [Test]
+    public void AddShortToMid()
+    {
+        timer.ChangeTimerValue(20);
+        int val = timer.AddSubtractTimerValue(50);
+        Assert.AreEqual(0, val);
+        Assert.AreEqual(70, timer.GetTime());
+        Assert.AreEqual(70, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void AddShortToLong()
+    {
+        timer.ChangeTimerValue(20);
+        int val = timer.AddSubtractTimerValue(1200);
+        Assert.AreEqual(2, val);
+        Assert.AreEqual(20, timer.GetTime());
+        Assert.AreEqual(20, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void AddMid()
+    {
+        timer.ChangeTimerValue(70);
+        int val = timer.AddSubtractTimerValue(10);
+        Assert.AreEqual(0, val);
+        Assert.AreEqual(80, timer.GetTime());
+        Assert.AreEqual(80, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void AddMidToLong()
+    {
+        timer.ChangeTimerValue(70);
+        int val = timer.AddSubtractTimerValue(1200);
+        Assert.AreEqual(2, val);
+        Assert.AreEqual(70, timer.GetTime());
+        Assert.AreEqual(70, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void AddLong()
+    {
+        timer.ChangeTimerValue(1200);
+        int val = timer.AddSubtractTimerValue(20);
+        Assert.AreEqual(2, val);
+        Assert.AreEqual(1200, timer.GetTime());
+        Assert.AreEqual(1200, timer.GetTotalTime());
+
+    }
+
+    [Test]
+    public void SubtractLong()
+    {
+        timer.ChangeTimerValue(1250);
+        int val = timer.AddSubtractTimerValue(-49);
+        Assert.AreEqual(2, val);
+        Assert.AreEqual(1250, timer.GetTime());
+        Assert.AreEqual(1250, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void SubtractLongToMid()
+    {
+        timer.ChangeTimerValue(1250);
+        int val = timer.AddSubtractTimerValue(-100);
+        Assert.AreEqual(0, val);
+        Assert.AreEqual(1150, timer.GetTime());
+        Assert.AreEqual(1150, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void SubtractLongToShort()
+    {
+        timer.ChangeTimerValue(1250);
+        int val = timer.AddSubtractTimerValue(-1200);
+        Assert.AreEqual(1, val);
+        Assert.AreEqual(1250, timer.GetTime());
+        Assert.AreEqual(1250, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void SubtractMid()
+    {
+        timer.ChangeTimerValue(300);
+        int val = timer.AddSubtractTimerValue(-50);
+        Assert.AreEqual(0, val);
+        Assert.AreEqual(250, timer.GetTime());
+        Assert.AreEqual(250, timer.GetTotalTime());
+    }
+
+    [Test]
+    public void SubtractMidToShort()
+    {
+        timer.ChangeTimerValue(300);
+        int val = timer.AddSubtractTimerValue(-260);
+        Assert.AreEqual(1, val);
+        Assert.AreEqual(300, timer.GetTime());
+        Assert.AreEqual(300, timer.GetTotalTime());
     }
 
 }
