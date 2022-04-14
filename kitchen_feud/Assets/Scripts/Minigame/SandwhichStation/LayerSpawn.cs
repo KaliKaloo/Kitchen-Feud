a2a8f19c20c9e8670e79f78afd417040dae6c1b5
Linda <fv19959@bristol.ac.uk>
2022-04-14 22:56:52 +0100

    sandwich fix + ui on main

--- kitchen_feud/Assets/Scripts/Minigame/SandwhichStation/LayerSpawn.cs
+++ kitchen_feud/Assets/Scripts/Minigame/SandwhichStation/LayerSpawn.cs
@@ -8,33 +8,40 @@ public class LayerSpawn : MonoBehaviour
     // Start is called before the first frame update
     public GameObject prefab;
     public SpriteAtlas imgAtlas;
-        
+    public Camera UICamera;
+    
+    private int chosenY;
+    private int chosenX;
     private float gridX = 1;
     private float gridY = 4;
-    private float spacing = Screen.height/6;
-
+    
     public void StartSpawn(List<string> idList)
     {
-            for (int y = 1; y <= gridY; y++)
+        chosenY = (int)(2f * UICamera.orthographicSize);
+        chosenX = (int)(chosenY  * UICamera.aspect);
+        float spacing = chosenY/6;
+   
+
+        for (int y = 1; y <= gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
             {
-                for (int x = 0; x < gridX; x++)
-                {
-                    GameObject myParentObject = gameObject;
-                    Vector3 newPosition = myParentObject.transform.position;
-                    newPosition.z = 0;
-                    myParentObject.transform.position = newPosition;
-          
+                GameObject myParentObject = gameObject;
+                Vector3 newPosition = myParentObject.transform.position;
+                newPosition.z = 0;
+                myParentObject.transform.position = newPosition;
+        
 
-                    Vector3 pos = new Vector3(Screen.width/2, y * spacing, 0) ;
-                    GameObject newObject = Instantiate(prefab, pos, Quaternion.identity);
-                    newObject.transform.SetParent(myParentObject.transform);
+                Vector3 pos = new Vector3(chosenX/2, y * spacing, 0) ;
+                GameObject newObject = Instantiate(prefab, pos, Quaternion.identity);
+                newObject.transform.SetParent(myParentObject.transform);
 
-                    newObject.GetComponent<SandwichMove>().LayerID = idList[y-1];
-                    newObject.GetComponent<SandwichMove>().speed = Random.Range(550, 730);
-                    newObject.GetComponent<SandwichMove>().imgAtlas = imgAtlas;
-                    transform.localPosition = new Vector3(0,0,0);
-                }
+                newObject.GetComponent<SandwichMove>().LayerID = idList[y-1];
+                newObject.GetComponent<SandwichMove>().speed = Random.Range(550, 730);
+                newObject.GetComponent<SandwichMove>().imgAtlas = imgAtlas;
+                transform.localPosition = new Vector3(0,0,0);
             }
+        }
         
     }
     
