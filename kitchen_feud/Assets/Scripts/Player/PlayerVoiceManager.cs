8804fed6a8d4c59f25424f281a1eba4d2053d7e6
Pragya Gurung <pragyagurung002@gmail.com>
2022-04-29 19:55:15 +0100

    changed push value

--- kitchen_feud/Assets/Scripts/Player/PlayerVoiceManager.cs
+++ kitchen_feud/Assets/Scripts/Player/PlayerVoiceManager.cs
@@ -296,7 +296,7 @@ public class PlayerVoiceManager : MonoBehaviour
 		Rigidbody rb = obj.GetComponent<Rigidbody>();
 		Vector3 direction = obj.transform.position - me.transform.position;
 		direction.y = 0;
-		rb.AddForce(direction * 5, ForceMode.Impulse);
+		rb.AddForce(direction * 2, ForceMode.Impulse);
 	}
 	[PunRPC]
 	void setStarted(int viewID,int x)
