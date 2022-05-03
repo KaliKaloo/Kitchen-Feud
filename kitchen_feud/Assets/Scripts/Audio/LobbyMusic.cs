c0988327b42dcec40fe78fedf197eb6ed62719e0
lokhei <lokhei01@gmail.com>
2022-05-03 20:11:12 +0100

    loop and connect lobby music

---
+++ kitchen_feud/Assets/Scripts/Audio/LobbyMusic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMusic : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    public AudioClip track1, track2;
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.volume = 0.03f;
+        audioSource.clip = track1;
+        audioSource.Play();
+        Invoke("loopTrack2", track1.length);
+    }
+
+
+    public void loopTrack2(){
+        audioSource.clip = track2;
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+
+}
