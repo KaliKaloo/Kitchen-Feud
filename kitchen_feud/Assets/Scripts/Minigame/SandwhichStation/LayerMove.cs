9cb03e0761dd1eac33cd5b01201f9a0633b2701c
Linda <fv19959@bristol.ac.uk>
2022-05-03 18:16:42 +0100

    sandwich fix - way better, not perfect

---
+++ kitchen_feud/Assets/Scripts/Minigame/SandwhichStation/LayerMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+    public class LayerMove : MonoBehaviour, IPointerClickHandler
+    {
+    
+        public SandwichMove SandwichMove;
+    
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // Debug.Log("clicked");
+            if ((!SandwichMove.stopped) && (SandwichMove.SandwichController.checkStoppedID(SandwichMove.LayerID))){
+                SandwichMove.stopped = true;
+                SandwichMove.SandwichController.moving = false;
+                SandwichMove.SandwichController.CountStopped++;
+                SandwichMove.StopMove();
+            }
+
+        }
+    }
+
