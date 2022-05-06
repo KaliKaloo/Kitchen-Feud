b7d97122a89296ba9d24469fcdbc3c91587f1184
Hamza Shahid <34235048+hamza772000@users.noreply.github.com>
2022-05-06 14:55:54 +0100

    tried fixing sandwich game points

--- kitchen_feud/Assets/Scripts/Minigame/SandwhichStation/SandwichStation.cs
+++ kitchen_feud/Assets/Scripts/Minigame/SandwhichStation/SandwichStation.cs
@@ -16,14 +16,16 @@ public class SandwichStation: MonoBehaviour
 
     void Start()
     {
-        GameEvents.current.assignPoints += UpdateDishPointsSandwich;
+      //  GameEvents.current.assignPoints += UpdateDishPointsSandwich;
         appliance = GetComponent<Appliance>();
+       
 
     }
     void Update()
     {
         if (appliance.isBeingInteractedWith && appliance.player && appliance.player.GetComponent<PhotonView>().IsMine)
         {
+            SandwichController.SandwichStation = GetComponent<SandwichStation>();
             MusicManager.instance.minigameSwitch();
 		    MusicManager.instance.inMG = true;
             backbutton.appliance = GetComponent<Appliance>();
@@ -42,20 +44,31 @@ public class SandwichStation: MonoBehaviour
     {
         if (appliance.isBeingInteractedWith)
         {
+        
             Dish dishOfFoundDish = appliance.dishOfFoundDish;
             if (dishOfFoundDish != null)
             {
 
-                dishOfFoundDish.GetComponent<PhotonView>().RPC("pointSync", RpcTarget.Others, SandwichController.finalScore);
-                int ingredientMultiplier = appliance.foundDish.recipe.Count - 1;
-                dishOfFoundDish.points = SandwichController.finalScore + (30 * ingredientMultiplier);
+                if (appliance.foundDish)
+                {
+                    int ingredientMultiplier = appliance.foundDish.recipe.Count - 1;
 
-                // if player is team 2 but interacts with team1 stove, points doubled
-                if (sandwichCanvas.tag == "Team1" && (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] == 2)
-                    dishOfFoundDish.points = dishOfFoundDish.points * 2;
-                // if player is team 1 but interacts with team2 stove, points doubled
-                else if (sandwichCanvas.tag == "Team2" && (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] == 1)
-                    dishOfFoundDish.points = dishOfFoundDish.points * 2;
+                    dishOfFoundDish.points = SandwichController.finalScore + (30 * ingredientMultiplier);
+
+                    // if player is team 2 but interacts with team1 stove, points doubled
+                    if (sandwichCanvas.tag == "Team1" && (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] == 2)
+                        dishOfFoundDish.points = dishOfFoundDish.points * 2;
+
+                    // if player is team 1 but interacts with team2 stove, points doubled
+                    else if (sandwichCanvas.tag == "Team2" && (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] == 1)
+                        dishOfFoundDish.points = dishOfFoundDish.points * 2;
+
+                    dishOfFoundDish.GetComponent<PhotonView>().RPC("pointSync", RpcTarget.Others, dishOfFoundDish.points);
+
+
+                }
+
+
 
             }
             else
